d1d2663b75ecfe3a5de31eeedbef8dc94253f4f9
Curtis Rubeck <rubeckcurtis@gmail.com>
2026-07-08 07:43:58 -0500

    add handlers for snippets

--- main.go
+++ main.go
@@ -11,11 +11,23 @@ func home(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+// Add a snippetView handler function.
+func snippetView(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
 func main() {
 	// Use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home) // Restrict this route to exact matches on / only.
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Print a log message to say the that server is starting.
 	log.Print("starting server on :4000")
