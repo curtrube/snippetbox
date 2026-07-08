3e1c919e4acc13dadd9d3f9edb8632bc02b68b72
Curtis Rubeck <rubeckcurtis@gmail.com>
2026-07-08 07:31:30 -0500

    hello http handler

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// Define a home handler function which writes a byte slice containing
+// "Hello from Snippetbox" as the response body.
+func home(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func main() {
+	// Use the http.NewServeMux() function to initialize a new servemux, then
+	// register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// Print a log message to say the that server is starting.
+	log.Print("starting server on :4000")
+
+	// Use the http.ListenAndServe() function to start a new web server. We pass
+	// in the two parameters: the TCP network address to list on (in this case ":4000")
+	// and the servemux we just created. If the http.ListenAndServe() returns an error
+	// we use log.Fatal() function to log the error message and terminiate the program.
+	// Note that any error returned by the http.ListenAndServe() is always non-nil.
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
