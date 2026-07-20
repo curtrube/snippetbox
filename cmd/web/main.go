a05c2b86f8d71b32e6b56d8c5db120a21ca7f4c7
Curtis Rubeck <rubeckcurtis@gmail.com>
2026-07-20 10:22:50 -0500

    add template cache so template are not read for each http call

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
@@ -11,67 +12,46 @@ import (
 	_ "github.com/go-sql-driver/mysql" // New import
 )
 
-// Add a snippets field to the application struct. This will allow us to
-// use the SnippetModel type in our handlers.
+// Add a templateCache field to the application struct.
 type application struct {
-	logger   *slog.Logger
-	snippets *models.SnippetModel
+	logger        *slog.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
-	// Define a new command-line flag with the name 'addr', a default value of ":4000"
-	// and some short help text explaining what the flag controls. The value of the
-	// flag will be stored in the addr variable at runtime.
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	// Define a new command-line flag for the MySQL DNS string.
 	dsn := flag.String("dsn", "web:pass@tcp(localhost:3306)/snippetbox?parseTime=true", "MySQL data source name")
-
-	// Importantly, we use the flag.Parse() function to parse the command-line flag.
-	// This read in the command-line flag value adn assigns it to the addr variable.
-	// You need to call this *before* you use the addr variable otherwise it will always
-	// contain the default value of ":4000". If any errors are encountered during parsing
-	// the application will be terminated.
 	flag.Parse()
 
-	// Use the slog.New() function to initialize a new structured logger, which writes
-	// to the standard out stream and uses the default settings.
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		// AddSource: true,
 		Level: slog.LevelDebug,
 	}))
 
-	// To kepp the main() function tidy I've put the code for creating a connection pool
-	// into the separate openDB() function below. We pass openDB() the DSN from the
-	// command-line flag.
 	db, err := openDB(*dsn)
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
-
-	// We also defer a call to db.Close(), so that the connection pool is closed before
-	// the main() function exits.
 	defer db.Close()
 
-	// Initialize a new instance of our application struct, containing the dependencies
-	// (for now, just the structured logger).
-
-	// Initialize a models.SnippetModel instance containing the DB connection pool and
-	// add it to the application dependencies.
-	app := &application{
-		logger:   logger,
-		snippets: &models.SnippetModel{DB: db},
+	// Initialize a new template cache...
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
 	}
 
-	// Use the Info() method to log the starting server message at Info severity
-	// (along with the listen address as an attribute).
+	// Add the templateCache to the application dependencies.
+	app := &application{
+		logger:        logger,
+		snippets:      &models.SnippetModel{DB: db},
+		templateCache: templateCache,
+	}
+
 	logger.Info("starting server", slog.String("addr", *addr))
 
 	err = http.ListenAndServe(*addr, app.routes())
-
-	// And we also use the Error() method to log any error message returned by
-	// http.ListenAndServe() at Error severity (with no additional attributes),
-	// and then call os.Exit(1) to terminate the application with exit code 1.
 	logger.Error(err.Error())
 	os.Exit(1)
 }
